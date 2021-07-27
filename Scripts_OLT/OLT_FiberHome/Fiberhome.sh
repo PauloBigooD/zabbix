398e13319a08ab06a47d1f77dc816b9845170cb9
PauloBigooD <53543357+PauloBigooD@users.noreply.github.com>
2021-07-27 20:21:11 -0300

    Update Fiberhome.sh

--- Scripts_OLT/OLT_FiberHome/Fiberhome.sh
+++ Scripts_OLT/OLT_FiberHome/Fiberhome.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Contato: pauloeduardojunior19@gmail.com
+# Script utilizado para coleta de informações do quantitativo das ONU's da OLT FiberHome
+# $1 - Modo de operação (total, slot, pon)
+# $2 - Endereço IP da OLT
+# $3 - Porta da comunicação SNMP, geralmete 161
+# $4 - Comunidade SNMP
+# $5 - Slot da OLT - 1, 2 ,3 ...
+# $6 - PON da OLT - 1, 2 ,3 ...
 
 if [ "$1" = "total" ]; then
         snmpbulkwalk -v2c -c $4 $2:$3 1.3.6.1.4.1.5875.800.3.10.1.1.11 | wc -l
@@ -17,5 +25,3 @@ if [ "$1" = "pon" ]; then
         #Obtém contagem de ONUs por PON
         snmpbulkwalk -v2c -c $4 $2:$3 1.3.6.1.4.1.5875.800.3.10.1.1.3 | egrep $list | grep ": $6$" | wc -l
 fi
-
-
